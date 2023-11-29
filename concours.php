fe80b0999605c5b072ab5720cf839798598265a0
RETR0EDIT <nolandme2003@gmail.com>
2023-11-30 00:57:55 +0100

    css concours

--- concours.php
+++ concours.php
@@ -1,22 +1,23 @@
   <?php include 'utile/nav.html'; ?>
   <img src="assets/img/page_1/banniere.png" alt="banniere" class="banniere">
-    <img src="assets/img/concours/IMG_5337.jpg" alt="img"  id="img_concours">
+  <div class="centre"><img src="assets/img/concours/IMG_5337.jpg" alt="img" class="widht80" id="img_concours"></div>
     <?php include 'utile/article_remi.html' ?>
     <h2>Comment se déroule le concour ? Antoine Imbault nous explique :</h2>
     <h2 class="centre">AUDIO</h2>
-  <audio src="" class="centre"></audio>
+    <div class="centre"><audio src="" class="centre"></audio></div>
   <h2>Retranscription audio</h2>
   <h2>Le point de vue  de Gaël Radigon membre du jury a propos du concours :</h2>
-  <video src="" controls class="centre"></video>
+  <div class="centre"><video src="" controls class="centre"></video></div>
   <h2>Le concours en vidéo :</h2>
-  <video src="" controls></video>
+  <div class="centre"><video src="" controls></video></div>
   <h2>La chanson de la confrérie du Brie de Meaux</h2>
   <h2 class="centre">AUDIO</h2>
-  <img src="" alt="" class="centre">
+  <div class="centre"><img src="assets/img/concours/IMG_9800.jpg" alt="" class="widht80"></div>
+  
   <p class="centre">Le jury lors de leur présentation au publique</p>
   <h2>Présentation du jury lors de l’intronisation</h2>
   <h2 class="centre">AUDIO</h2>
-  <audio src=""></audio>
+  <div class="centre"><audio src=""></audio></div>
     <?php include 'utile/footer.html' ?>
     <script src="assets/js/translate.js"></script>
     <script src="assets/js/fade-up.js"></script>
