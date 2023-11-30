8f2b61e970ef650d045eab8a8ee5a313a734f529
RETR0EDIT <nolandme2003@gmail.com>
2023-11-30 01:04:01 +0100

    html

--- concours.php
+++ concours.php
@@ -1,4 +1,4 @@
-  <?php include 'utile/nav.html'; ?>
+<?php include 'utile/nav.html'; ?>
   <img src="assets/img/page_1/banniere.png" alt="banniere" class="banniere">
   <div class="centre"><img src="assets/img/concours/IMG_5337.jpg" alt="img" class="widht80" id="img_concours"></div>
     <?php include 'utile/article_remi.html' ?>
